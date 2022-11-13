833c47f9ca86ef25c6a89eeddb53e2627065d04b
mitchellabrouwer <mbarron198@gmail.com>
2022-11-13 19:53:39 +1100

    fix: email transport creation

--- pages/api/stripe/success.ts
+++ pages/api/stripe/success.ts
@@ -1,14 +1,20 @@
+/* eslint-disable import/no-anonymous-default-export */
 import nodemailer from "nodemailer";
 import prisma from "../../../lib/prisma";
 
-/* eslint-disable import/no-anonymous-default-export */
 export default async (req, res) => {
   if (req.method !== "POST") {
-    return res.status(405).end();
+    res.status(405).end(); //Method Not Allowed
+    return;
   }
 
   const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-  const stripe_session = await stripe.checkout.sessions.retrieve(req.body.session_id);
+  const stripe_session = await stripe.checkout.sessions.retrieve(req.body.session_id, {
+    expand: ["line_items"],
+  });
+
+  console.log(stripe_session);
+  console.log(process.env.EMAIL_SERVER);
 
   await prisma.order.create({
     data: {
@@ -19,36 +25,44 @@ export default async (req, res) => {
     },
   });
 
-  const transporter = nodemailer.createTransport(process.env.EMAIL_SERVER);
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_SERVER_HOST,
+    port: Number(process.env.EMAIL_SERVER_PORT),
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_SERVER_USER,
+      pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
+  });
 
   let bodyToOwner = `
-  <p>New order</p>
-  <p>Total paid $${stripe_session.amount_total / 100}</p>
+      <p>New order</p>
+      <p>Total paid $${stripe_session.amount_total / 100}</p>
 
 
-  <p>Customer: ${stripe_session.customer_details.name}</p>
-  <p>Email: ${stripe_session.customer_details.email}</p>
-  <p>Address: ${stripe_session.customer_details.address.line1} ${
+      <p>Customer: ${stripe_session.customer_details.name}</p>
+      <p>Email: ${stripe_session.customer_details.email}</p>
+      <p>Address: ${stripe_session.customer_details.address.line1} ${
     stripe_session.customer_details.address.city
   } ${stripe_session.customer_details.address.country}</p>
 
-  <p>Products:</p>
+      <p>Products:</p>
 
-  `;
+      `;
 
-  stripe_session.display_items.map((item) => {
-    bodyToOwner += `<p>${item.quantity} ${item.custom.name}</p>`;
+  stripe_session.line_items.data.map((item) => {
+    bodyToOwner += `<p>${item.quantity} ${item.description}</p>`;
   });
 
   let bodyToCustomer = `
-  <p>Thanks for your order!</p>
- 
-  <p>Products bought:</p>
+      <p>Thanks for your order!</p>
+     
+      <p>Products bought:</p>
 
-  `;
+      `;
 
-  stripe_session.display_items.map((item) => {
-    bodyToCustomer += `<p>${item.quantity} ${item.custom.name}</p>`;
+  stripe_session.line_items.data.map((item) => {
+    bodyToCustomer += `<p>${item.quantity} ${item.description}</p>`;
   });
 
   bodyToCustomer += `<p>We'll ship those as soon as possible</p>`;
@@ -81,5 +95,5 @@ export default async (req, res) => {
     }
   );
 
-  return res.end();
+  res.end();
 };
